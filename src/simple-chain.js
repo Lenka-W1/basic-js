1f7274b7dc6983fba293f9342b12bc9d31d998d0
lenka-w1 <lenka-w1@mail.ru>
2021-10-04 21:59:45 +0300

    feat: add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,31 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+   
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.arr.length;  
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.arr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(isNaN(position) || position <= 0 || position > this.arr.length){
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.arr.join('~~');
+    this.arr = [];
+    return chain;  
   }
 };
